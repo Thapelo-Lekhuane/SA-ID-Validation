24547e462a45ebf844719b3d5a8f4359e3afa397
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-04-23 14:53:31 +0200

    feat: add ValidateSaId class with isIdNumberValid method stub

---
+++ app/src/main/java/org/example/ValidateSaId.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ValidateSaId {
+    public static boolean isIdNumberValid(String idNumber) {
+        // TODO: Implement validation logic
+        return false;
+    }
+}
